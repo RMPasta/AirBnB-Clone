2260de3f0bc3028412f9e3a346fc2203f7d362e5
rmpasta <rmpasta.code@gmail.com>
2023-03-21 14:08:41 -0400

    add git spots owened by current user need to test auth

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -22,38 +22,20 @@ const router = express.Router();
 //     handleValidationErrors
 //   ];
 
-//   router.post(
-//     '/',
-//     validateLogin,
-//     async (req, res, next) => {
+//   router.post('/', validateLogin, async (req, res, next) => {
 //       const { credential, password } = req.body;
 
-//       const user = await User.unscoped().findOne({
-//         where: {
-//           [Op.or]: {
-//             username: credential,
-//             email: credential
-//           }
-//         }
+//       const spot = await Spot.create({
+
 //       });
 
-//       if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-//         const err = new Error('Invalid credentials');
-//         err.status = 401;
-//         err.title = 'Login failed';
-//         err.errors = { credential: 'The provided credentials were invalid.' };
-//         return next(err);
-//       }
-
-//       const safeUser = {
+//       const newSpot = {
 //         id: user.id,
 //         firstName: user.firstName,
 //         lastName: user.lastName,
 //         email: user.email,
 //         username: user.username,
 //       };
-
-//       await setTokenCookie(res, safeUser);
 
 //       return res.json({
 //         user: safeUser
@@ -107,5 +89,10 @@ const router = express.Router();
     }
   );
 
+  router.get('/current', async (req, res) => {
+    const currentUserId = req.user.id;
+    let spot = await Spot.findOne({where: {ownerId: currentUserId}});
+    res.json(spot)
+  })
 
   module.exports = router;
