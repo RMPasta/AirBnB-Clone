c45803fd0dd13aeb574a067f3b83297ef6c5fe55
rmpasta <rmpasta.code@gmail.com>
2023-03-21 20:14:35 -0400

    trouble shoot auth status codes

--- backend/db/migrations/20230316215301-create-user.js
+++ backend/db/migrations/20230316215301-create-user.js
@@ -18,7 +18,6 @@ module.exports = {
       username: {
         type: Sequelize.STRING(30),
         allowNull: false,
-        unique: true
       },
       firstName: {
         type: Sequelize.STRING(30),
