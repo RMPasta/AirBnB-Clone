e485ce636550ba74eabe6113d7894fb3d0d5d24e
rmpasta <rmpasta.code@gmail.com>
2023-04-07 14:05:20 -0400

    add and style sign up component and the thunk for it

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,30 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
@@ -38,8 +62,6 @@ const sessionReducer = (state = initialState, action) => {
     case SET_USER:
       newState = Object.assign({}, state);
       newState.user = action.payload;
-      newState.user.createdAt = new Date().toJSON()
-      newState.user.updatedAt = new Date().toJSON()
       return newState;
     case REMOVE_USER:
       newState = Object.assign({}, state);
