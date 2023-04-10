80107111b84f57441693c29b3446e2395acaa1d9
rmpasta <rmpasta.code@gmail.com>
2023-04-10 00:36:33 -0400

    add styling and set cards to wireframe

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -13,12 +13,19 @@ export default function Landing() {
   return (
     <div className="page">
           {spots && spots.map((spot) => (
-         <li key={spot.id}>
+         <li key={spot.id} className="spot-card">
             <img src={spot.previewImage} alt={spot.description} className="previewImage" />
-            <h1>{spot.name}</h1>
-            <h2>{spot.id}</h2>
-            <h3>{spot.description}</h3>
-            <p>{spot.price}</p>
+            <div className="name-rating-container">
+              <p>{spot.city}, {spot.state}</p>
+              <div className="rating">
+                <p className="avgRating">{spot.avgRating}</p>
+                <i className="fas fa-star"></i>
+              </div>
+            </div>
+            <div className="price">
+            <p>${spot.price} night</p>
+            </div>
+            {/* <h4>{spot.id}</h4> */}
          </li>
         ))}
     </div>
