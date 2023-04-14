6906469ea0ab937b358f0f9d511bf06522f8e147
RMPasta <rmpasta.code@gmail.com>
2023-04-14 09:28:39 -0400

    add console log to trouble shoot

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,44 +1,57 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { getSpotsThunk } from '../../store/spots';
-import { nanoid } from 'nanoid';
-import './Landing.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { getSpotsThunk } from "../../store/spots";
+import { nanoid } from "nanoid";
+import "./Landing.css";
 
 export default function Landing() {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const spotsObj = useSelector(state=>state.spots); // populate from Redux store
-    const spots = Object.values(spotsObj);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const spotsObj = useSelector((state) => state.spots); // populate from Redux store
+  const spots = Object.values(spotsObj);
+  console.log(spots);
+  const handleClick = (spot) => {
+    history.push(`/spots/${spot.id}`);
+  };
 
-    const handleClick = (spot) => {
-      history.push(`/spots/${spot.id}`)
-    }
-
-    useEffect(() => {
-        dispatch(getSpotsThunk())
-      }, [dispatch]);
+  useEffect(() => {
+    dispatch(getSpotsThunk());
+  }, [dispatch]);
 
   return (
     <div className="page">
-          {spots && spots.map((spot) => (
-         <li key={nanoid(5)} className="spot-card">
+      {spots &&
+        spots
+          .map((spot) => (
+            <li key={nanoid(5)} className="spot-card">
               <div className="clickable" onClick={() => handleClick(spot)}>
-          <div className='tool-tip'>{spot && spot.name}</div>
-            <img src={spot && spot.previewImage} alt={spot && spot.description} className="previewImageLanding" />
-            <div className="name-rating-container">
-              <p>{spot && spot.city}, {spot && spot.state}</p>
-              <div className="rating">
-                { spot && <p className="avgRating">{spot.avgRating > 0 ? spot.avgRating : "New"}</p> }
-                <i className="fas fa-star"></i>
+                <div className="tool-tip">{spot && spot.name}</div>
+                <img
+                  src={spot && spot.previewImage}
+                  alt={spot && spot.description}
+                  className="previewImageLanding"
+                />
+                <div className="name-rating-container">
+                  <p>
+                    {spot && spot.city}, {spot && spot.state}
+                  </p>
+                  <div className="rating">
+                    {spot && (
+                      <p className="avgRating">
+                        {spot.avgRating > 0 ? spot.avgRating : "New"}
+                      </p>
+                    )}
+                    <i className="fas fa-star"></i>
+                  </div>
+                </div>
+                <div className="price">
+                  <p>${spot && spot.price} night</p>
+                </div>
               </div>
-            </div>
-            <div className="price">
-            <p>${spot && spot.price} night</p>
-            </div>
-            </div>
-         </li>
-        )).reverse()}
+            </li>
+          ))
+          .reverse()}
     </div>
-  )
+  );
 }
