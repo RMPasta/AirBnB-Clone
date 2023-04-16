3a3435e4518975124aa600237f3d426cfcf953e9
rmpasta <rmpasta.code@gmail.com>
2023-04-16 12:16:22 -0400

    break reviews out into component and adjust spot details logic

---
+++ frontend/src/components/ReviewsSection/index.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import OpenModalButton from '../OpenModalButton';
+import AddReviewModal from '../AddReviewModal';
+import DeleteReviewModal from '../DeleteReviewModal';
+import { useSelector } from 'react-redux';
+import { nanoid } from 'nanoid';
+
+export default function ReviewsSection({ spot, reviews}) {
+    const [reviewed, setReviewed] = useState(false);
+    const sessionUser = useSelector(state=>state.session.user);
+
+    useEffect(() => {
+      setReviewed(false)
+      reviews.forEach((review) => {
+        if (review.User && sessionUser && review.User.id === parseInt(sessionUser.id)) {
+          setReviewed(true);
+        }
+      })
+    }, [reviews, sessionUser])
+
+    if (!spot) return <div>...Loading</div>
+    if (!reviews) return <div>...Loading</div>
+
+    const sortedReviews = reviews.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+
+    const months = {
+      '01': 'January',
+      '02': 'February',
+      '03': 'March',
+      '04': 'April',
+      '05': 'May',
+      '06': 'June',
+      '07': 'July',
+      '08': 'August',
+      '09': 'September',
+      '10': 'October',
+      '11': 'November',
+      '12': 'December'
+    };
+
+  return (
+    <div className='spot-reviews'>
+    <div className='rating-reviews-section'>
+          {spot.avgStarRating && <p>{spot.numReviews} {spot.numReviews > 1 ? "reviews" : "review"}</p>}
+            <div className='rating'>
+              <p className="avgRating">{spot.avgStarRating ? spot.avgStarRating + " · " : "New"}</p>
+              <i className="fas fa-star"></i>
+            </div>
+          </div>
+          {!reviewed && sessionUser && spot.ownerId !== parseInt(sessionUser.id) && <OpenModalButton
+          buttonText="Add Review"
+          className="add-review-button"
+          modalComponent={<AddReviewModal spot={spot} />} /> }
+          {reviews.length < 1 && sessionUser && spot.ownerId !== parseInt(sessionUser.id) && <p className='be-the-first'>Be the first to post a review!</p>}
+
+      {sortedReviews && sortedReviews.map((review) => {
+        return <li key={nanoid(5)}  className='review'>
+          <div className='review-name'>{review.User ? review.User.firstName : sessionUser.firstName}</div>
+          <div className='review-date'>{months[review.createdAt.split('T')[0].slice(0, 7).split('-')[1]] + ' ' + review.createdAt.split('T')[0].slice(0, 7).split('-')[0]}</div>
+          <div>{review.review}</div>
+          { review.User && sessionUser && review.User.id === parseInt(sessionUser.id) && <OpenModalButton
+          className="delete-review-button"
+          buttonText="Delete Review"
+          modalComponent={<DeleteReviewModal spot={spot} />} /> }
+        </li>
+      })}
+    </div>
+  )
+}
--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -1,64 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import OpenModalButton from '../OpenModalButton';
+import ReviewsSection from '../ReviewsSection';
 import { nanoid } from 'nanoid';
 import './SpotDetails.css';
-import AddReviewModal from '../AddReviewModal';
-import DeleteReviewModal from '../DeleteReviewModal';
+
 
 export default function SpotDetails() {
   const spot = useSelector(state=>state.spots.spot);
   const reviews = useSelector(state=>Object.values(state.reviews));
-  const sessionUser = useSelector(state=>state.session.user);
-  const [reviewed, setReviewed] = useState(false);
-
-  useEffect(() => {
-    setReviewed(false)
-    reviews.forEach((review) => {
-      if (review.User && sessionUser && review.User.id === parseInt(sessionUser.id)) {
-        setReviewed(true);
-      }
-    })
-  }, [reviews, sessionUser])
 
   if (!spot) return <div>...Loading</div>
-  if (!reviews) return <div>...Loading</div>
-
-  const sortedReviews = reviews.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-
-  const months = {
-    '01': 'January',
-    '02': 'February',
-    '03': 'March',
-    '04': 'April',
-    '05': 'May',
-    '06': 'June',
-    '07': 'July',
-    '08': 'August',
-    '09': 'September',
-    '10': 'October',
-    '11': 'November',
-    '12': 'December'
-  };
 
   return (
     <div className='spot-details-page'>
-        { spot && <h1>{spot.name}</h1> }
-        { spot && <div>{spot.city}, {spot.state}, {spot.country}</div> }
-        {spot && spot.previewImage && <img src={spot.previewImage} alt={spot.name} /> }
+        <h1>{spot.name}</h1>
+        <div>{spot.city}, {spot.state}, {spot.country}</div>
+        {spot.previewImage && <img src={spot.previewImage} alt={spot.name} /> }
         <div className='spot-images-container'>
-          {spot && spot.SpotImages && spot.SpotImages.map((image, i) => {
+          {spot.SpotImages && spot.SpotImages.map((image, i) => {
             return <img key={nanoid(5)} src={image.url} alt={spot.name} className={image.preview ? `previewImage` : `spotImage spotImage-${i}`} />
           })}
         </div>
         <div className='spot-info'>
           <div>
             {spot.Owner && <h2>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2> }
-            {spot && <p>{spot.description}</p>}
+            <p>{spot.description}</p>
           </div>
           <div className='stat-block'>
             <div className="top-stats">
-              {spot && <p className='detail-price'>${spot.price} night</p>}
+              <p className='detail-price'>${spot.price} night</p>
                 <div className='rating'>
                 {spot.avgStarRating && <p>{spot.numReviews} {spot.numReviews > 1 ? "reviews" : "review"}</p> }
                   <p className="avgRating">{spot.avgStarRating ? spot.avgStarRating + " · " : "New"}</p>
@@ -70,31 +40,7 @@ export default function SpotDetails() {
             </div>
           </div>
         </div>
-        <div className='spot-reviews'>
-          <div className='rating-reviews-section'>
-                {spot.avgStarRating && <p>{spot.numReviews} {spot.numReviews > 1 ? "reviews" : "review"}</p>}
-                  <div className='rating'>
-                    <p className="avgRating">{spot.avgStarRating ? spot.avgStarRating + " · " : "New"}</p>
-                    <i className="fas fa-star"></i>
-                  </div>
-                </div>
-                {!reviewed && sessionUser && spot.ownerId !== parseInt(sessionUser.id) && <OpenModalButton
-                buttonText="Add Review"
-                className="add-review-button"
-                modalComponent={<AddReviewModal spot={spot} />} /> }
-                {reviews.length < 1 && sessionUser && spot.ownerId !== parseInt(sessionUser.id) && <p className='be-the-first'>Be the first to post a review!</p>}
-            {sortedReviews && sortedReviews.map((review) => {
-              return <li key={nanoid(5)}  className='review'>
-                <div className='review-name'>{review.User ? review.User.firstName : sessionUser.firstName}</div>
-                <div className='review-date'>{months[review.createdAt.split('T')[0].slice(0, 7).split('-')[1]] + ' ' + review.createdAt.split('T')[0].slice(0, 7).split('-')[0]}</div>
-                <div>{review.review}</div>
-                { review.User && sessionUser && review.User.id === parseInt(sessionUser.id) && <OpenModalButton
-                className="delete-review-button"
-                buttonText="Delete Review"
-                modalComponent={<DeleteReviewModal spot={spot} />} /> }
-              </li>
-            })}
-          </div>
+        <ReviewsSection spot={spot} reviews={reviews} />
     </div>
   )
 }
